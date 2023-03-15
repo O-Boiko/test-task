178dce402a35d9f75d13d5c22acae7343b07c743
Olha <boiko.olha@officetransform.com>
2023-03-15 12:54:46 +0200

    made App component more readable

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,18 @@
 import { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import Box from '@mui/material/Box';
-import FormLabel from '@mui/material/FormLabel';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import TextField from '@mui/material/TextField';
-import FormGroup from '@mui/material/FormGroup';
-import Checkbox from '@mui/material/Checkbox';
-import Pagination from '@mui/material/Pagination';
+import { useDispatch } from 'react-redux';
 
 import { Search } from './components/Search/Search';
 import { SortByRadioGroup } from './components/SortBy/SortBy';
-import { ProductCard } from './components/ProductCard/ProductCard';
-import { PRODUCTS_PER_PAGE } from './constants';
-import s from './App.module.scss';
+import { ColorFilter } from './components/ColorFilter/ColorFilter';
+import { PriceFilter } from './components/PriceFilter/PriceFilter';
+import { ProductsSection } from './components/ProductsSection/ProductsSection';
 
-import {
-  getPendingSelector,
-  getProductsSelector,
-  getErrorSelector
-} from './store/products/selectors';
 import { fetchProductsRequest } from './store/products/actions';
 import { Color, SortBy } from './store/products/types';
+import s from './App.module.scss';
 
 function App() {
   const dispatch = useDispatch();
-  const pending = useSelector(getPendingSelector);
-  const productsData = useSelector(getProductsSelector);
-  const error = useSelector(getErrorSelector);
   const [searchString, setSearchString] = useState<string>('');
   const [minPrice, setMinPrice] = useState<number | null>(null);
   const [maxPrice, setMaxPrice] = useState<number | null>(null);
@@ -70,81 +56,17 @@ function App() {
       <SortByRadioGroup sortByCallback={setSortBy} />
       <div className={s.main}>
         <aside>
-          <Box component="div">
-            <FormLabel>Color</FormLabel>
-            <FormGroup>
-              {Object.values(Color).map((color) => (
-                <FormControlLabel
-                  onChange={(e: any) => {
-                    const colorsFilter = e.target?.checked
-                      ? [color, ...colors]
-                      : colors.filter((e) => e !== color);
-                    setColors(colorsFilter);
-                  }}
-                  control={<Checkbox />}
-                  label={color}
-                  key={color}
-                />
-              ))}
-            </FormGroup>
-          </Box>
-          <Box component="div">
-            <FormLabel>Price</FormLabel>
-            <Box component="div">
-              <TextField
-                id="min"
-                {...(minPrice ? { value: minPrice } : {})}
-                onChange={(e: any) =>
-                  e.target?.value >= 0 && setMinPrice(e.target?.value)
-                }
-                onKeyDown={keyPress}
-                label="min"
-                variant="filled"
-                type="number"
-                size="small"
-                className={s.price}
-              />
-              <TextField
-                id="max"
-                {...(maxPrice ? { value: maxPrice } : {})}
-                onChange={(e: any) =>
-                  e.target?.value >= 0 && setMaxPrice(e.target?.value)
-                }
-                onKeyDown={keyPress}
-                label="max"
-                variant="filled"
-                type="number"
-                size="small"
-                className={s.price}
-              />
-            </Box>
-          </Box>
+          <ColorFilter colors={colors} setColors={setColors} />
+          <PriceFilter
+            minPrice={minPrice}
+            setMinPrice={setMinPrice}
+            maxPrice={maxPrice}
+            onKeyDown={keyPress}
+            setMaxPrice={setMaxPrice}
+          />
         </aside>
         <section className={s.productsSection}>
-          {productsData?.products?.length ? (
-            <div className={s.products}>
-              {productsData?.products?.map((product) => (
-                <ProductCard {...product} key={product.id} />
-              ))}
-            </div>
-          ) : (
-            ''
-          )}
-          {pending ? <div>Loading...</div> : ''}
-          {error ? <div>Something went wrong...</div> : ''}
-          {productsData?.products?.length ? (
-            <Pagination
-              count={Math.ceil(productsData.totalProducts / PRODUCTS_PER_PAGE)}
-              page={productsData.page + 1}
-              onChange={(e: React.ChangeEvent<unknown>, page: number) =>
-                makeDispatch(page - 1)
-              }
-              size="small"
-              className={s.pagination}
-            />
-          ) : (
-            ''
-          )}
+          <ProductsSection makeDispatch={makeDispatch} />
         </section>
       </div>
     </div>
---
+++ src/components/PriceFilter/PriceFilter.tsx
@@ -0,0 +1,52 @@
+﻿import Box from '@mui/material/Box';
+import FormLabel from '@mui/material/FormLabel';
+import TextField from '@mui/material/TextField';
+import s from './style.module.scss';
+
+interface PriceFilterProps {
+  minPrice: number | null;
+  maxPrice: number | null;
+  setMinPrice: (val: number) => void;
+  setMaxPrice: (val: number) => void;
+  onKeyDown: (event: any) => void;
+}
+
+export const PriceFilter: React.FC<PriceFilterProps> = ({
+  minPrice,
+  setMinPrice,
+  maxPrice,
+  onKeyDown,
+  setMaxPrice
+}) => (
+  <Box component="div">
+    <FormLabel>Price</FormLabel>
+    <Box component="div">
+      <TextField
+        id="min"
+        {...(minPrice ? { value: minPrice } : {})}
+        onChange={(e: any) =>
+          e.target?.value >= 0 && setMinPrice(e.target?.value)
+        }
+        onKeyDown={onKeyDown}
+        label="min"
+        variant="filled"
+        type="number"
+        size="small"
+        className={s.price}
+      />
+      <TextField
+        id="max"
+        {...(maxPrice ? { value: maxPrice } : {})}
+        onChange={(e: any) =>
+          e.target?.value >= 0 && setMaxPrice(e.target?.value)
+        }
+        onKeyDown={onKeyDown}
+        label="max"
+        variant="filled"
+        type="number"
+        size="small"
+        className={s.price}
+      />
+    </Box>
+  </Box>
+);
